4f3f5dc57c81603dbed103865d26f797ba4548f7
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-03-30 23:04:49 +0700

    Complete section 7

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
@@ -19,23 +19,32 @@ export default ({ data }) => {
             </h1>
             <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
             {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div key={node.id}>
-                <h3
-                css={css`
-                    margin-bottom: ${rhythm(1 / 4)};
-                `}
-                >
-                {node.frontmatter.title}{" "}
-                <span
-                    css={css`
-                    color: #bbb;
-                    `}
-                >
-                    — {node.frontmatter.date}
-                </span>
-                </h3>
-                <p>{node.excerpt}</p>
-            </div>
+                <div key={node.id}>
+                    <Link
+                        to={node.fields.slug}
+                        css={css`
+                            text-decoration: none;
+                            color: inherit;
+                        `
+                        }
+                    >
+                        <h3
+                        css={css`
+                            margin-bottom: ${rhythm(1 / 4)};
+                        `}
+                        >
+                        {node.frontmatter.title}{" "}
+                        <span
+                            css={css`
+                            color: #bbb;
+                            `}
+                        >
+                            — {node.frontmatter.date}
+                        </span>
+                        </h3>
+                        <p>{node.excerpt}</p>
+                    </Link> 
+                </div>
             ))}
         </div>
         </Layout>
@@ -53,6 +62,9 @@ export const query = graphql`
                 title
                 date(formatString: "DD MMMM, YYYY")
             }
+            fields {
+                slug
+            }    
             excerpt
         }
       }
